a05cfda500f3e92aa4c6edb30cc2c2013dca3ab4
Nick Vatamaniuc <vatamane@apache.org>
2016-08-31 22:46:20 -0400

    Actually GH is probably less flaky than our PyPI so just use that
    And avoid the whole knife bag dance...

--- dynoexample/build.sh
+++ dynoexample/build.sh
@@ -19,32 +19,11 @@ fi
 
 
 if [ ! -f ./venv/bin/rep ]; then
-
-    if [ -z "${SAPI_CI_USER+x}" ]; then
-        echo "SAPI_CI_USER not set. Using cloudant_sapi_ci"
-        SAPI_CI_USER=cloudant_sapi_ci
-    else
-        echo "Using found SAPI_CI_USER=${SAPI_CI_USER}"
-    fi
-
-    if [ -z "${SAPI_CI_PASSWD+x}" ]; then
-        echo "SAPI_CI_PASSWD not defined. Trying to get it from knife data bag..."
-        SAPI_CI_PASSWD=`knife data bag show -F json cloudant_sapi ci_user 2> /dev/null | python -c "import sys,json; d=json.loads(sys.stdin.read()); print d['web_password']"`
-        if [ -z  "${SAPI_CI_PASSWD+x}" ]; then
-            echo "ERROR: Could not get SAPI_CI_PASSWD from cloudant_sapi ci_user data bag"
-            echo "For development if ~/.clou file is present can set SAPI_CI_USER adn SAPI_CI_PASSWD from web user and password"
-            exit 1
-        fi
-        echo "Got pypi SAPI_CI_PASSWD from knife data bag."
-    else
-        echo "Using found SAPI_CI_PASSWD"
-    fi
-
     rm -rf venv
     virtualenv venv
     . ./venv/bin/activate
     pip install -q --upgrade pip
-    pip install -q -i "https://${SAPI_CI_USER}:${SAPI_CI_PASSWD}@pypi.cloudant.com/simple" -r requirements.txt
+    pip install -q -r requirements.txt
 else
     . ./venv/bin/activate
 fi
