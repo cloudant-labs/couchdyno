b8f8b2f2c3bd290c2d625b50cef8034593b3734f
Nick Vatamaniuc <vatamane@apache.org>
2017-01-06 11:02:13 -0500

    Bump version to 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="couchdyno",
-    version="1.0.0",
+    version="1.0.1",
     url="https://github.com/cloudant-labs/couchdyno",
     author="Nick Vatamaniuc",
     author_email="nvatama@us.ibm.com",
